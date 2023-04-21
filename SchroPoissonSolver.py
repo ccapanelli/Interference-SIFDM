5024fce33a41d590a8f1f9ae215644ea75f2eab9
ccapanelli <65098799+ccapanelli@users.noreply.github.com>
2023-04-21 16:21:10 -0400

    Add files via upload
    Updated to accept user-defined normalization.

--- SchroPoissonSolver.py
+++ SchroPoissonSolver.py
@@ -3,38 +3,39 @@ Create Your Own Quantum Mechanics Simulation (With Python)
 Philip Mocz (2020) Princeton Univeristy, @PMocz
 Simulate the Schrodinger-Poisson system with the Spectral method
 
-Include self-interactions, calculate density contrast, and center plots around solitons
+Include self-interactions, calculate density contrast, and center plots around cores
 Christian Capanelli (2022), McGill University
 """
 
+import numpy as np
 
-def fdmSimulation(rho0, g_SI, tEnd, dt):
-    import numpy as np
+def fdmSimulation(rho0, normalization, G_N, g_SI, tEnd, dt, Nt_saved):
+    
     """ Quantum simulation """
 
     # Simulation parameters
     N         = int(np.sqrt(rho0.size))    # Spatial resolution
     t         = 0      # current time of the simulation
     tOut      = 0.0001  # draw frequency
-    G         = 4000  # Gravitaitonal constant
-    g         = g_SI*G #1e-2*np.pi*G # self-interaction strength
+    G         = G_N # Gravitaitonal constant
+    g         = g_SI #1e-2*np.pi*G # self-interaction strength
     plotRealTime = False # switch on for plotting as the simulation goes along
 
     # Domain [0,1] x [0,1]
     L = 1    
     xlin = np.linspace(0,L, num=N+1)  # Note: x=0 & x=1 are the same point!
     xlin = xlin[0:N]                     # chop off periodic point
-    xx, yy = np.meshgrid(xlin, xlin)
+    xx, yy = np.meshgrid(xlin, xlin, copy=False)
 
-    # normalize wavefunction to <|psi|^2>=1
+    # normalize wavefunction to <|psi|^2>=rho / (m psi_0^2)
     rhobar = np.mean( rho0 )
-    rho0 /= rhobar
+    rho0 = rho0/rhobar*normalization
     psi = np.sqrt(rho0)
 
 
     # Fourier Space Variables
     klin = 2.0 * np.pi / L * np.arange(-N/2, N/2)
-    kx, ky = np.meshgrid(klin, klin)
+    kx, ky = np.meshgrid(klin, klin, copy=False)
     kx = np.fft.ifftshift(kx)
     ky = np.fft.ifftshift(ky)
     kSq = kx**2 + ky**2
@@ -48,17 +49,12 @@ def fdmSimulation(rho0, g_SI, tEnd, dt):
     
     # create array to store contrast field at each time step
     a , b = np.shape(psi)
-    psiFieldSlices = np.zeros((Nt , a, b))
+#     rhoAvg = np.zeros((a,b))
+    psiFieldSlices = np.zeros(( Nt_saved , a, b), dtype = complex)
     contrastVals = np.zeros((Nt))
-    phases = np.zeros((Nt , a, b))
+#     phases = np.zeros((Nt , a, b))
     
-    # prep figure
-#     fig = plt.figure(figsize=(6,4), dpi=100)
-#     grid = plt.GridSpec(1, 2, wspace=0.0, hspace=0.0)
-#     ax1 = plt.subplot(grid[0,0])
-#     ax2 = plt.subplot(grid[0,1])
-#     outputCount = 1
-
+    j=0
     # Simulation Main Loop
     for i in range(Nt):
         # (1/2) kick
@@ -75,58 +71,25 @@ def fdmSimulation(rho0, g_SI, tEnd, dt):
 
         # (1/2) kick
         psi = np.exp(-1.j*dt/2.0*V) * psi
-
-        psiFieldSlices[i, :,:] = np.abs(psi)**2
+        
+        # record last few field configurations
+        
+        if i > (Nt/5) and (i)%(Nt/(5*Nt_saved)) < 1. and j < Nt_saved:
+            psiFieldSlices[j,:,:] = psi
+            j = j + 1
+            
+        #rhoAvg += np.abs(psi)**2
+        # psiFieldSlices[i, :,:] = np.abs(psi)**2
         contrastVals[i] = np.std(np.abs(psi)**2)/np.average(np.abs(psi)**2)
-        phases[i,:,:] = np.angle(psi)
+        # phases[i,:,:] = np.angle(psi)
         
         # update time
         t += dt
-
-#         # plot in real time
-#         plotThisTurn = False
-#         if t + dt > outputCount*tOut:
-#             plotThisTurn = True
-#         if (plotRealTime and plotThisTurn) or (i == Nt-1):
-
-#             plt.sca(ax1)
-#             plt.cla()
-
-#             plt.imshow(np.log10(np.abs(psi)**2), cmap = 'inferno')
-#             plt.clim(-1, 2)
-#             ax1.get_xaxis().set_visible(False)
-#             ax1.get_yaxis().set_visible(False)
-#             ax1.set_aspect('equal')
-
-#             plt.sca(ax2)
-#             plt.cla()
-#             plt.imshow(np.angle(psi), cmap = 'bwr')
-#             plt.clim(-np.pi, np.pi)
-#             ax2.get_xaxis().set_visible(False)
-#             ax2.get_yaxis().set_visible(False)
-#             ax2.set_aspect('equal')
         
+    psiFieldSlices[-1,:,:] = psi
 
-#             plt.pause(0.001)
-#             outputCount += 1
 
-#     # Plot density contrast
-#     plt.figure(dpi=150)
-#     plt.title(r"$\mathrm{SI}/\mathrm{Gravity}=$ "+str(g_SI))
-#     plt.plot(contrastVals)
-#     plt.xlabel("time")
-#     plt.ylabel(r"$\delta\rho/\rho$", rotation=45)
-#     plt.show()
-    
-#     # Save figure
-#     plt.sca(ax1)
-#     plt.title(r'$\log_{10}(|\psi|^2)$')
-#     plt.sca(ax2)
-#     plt.title(r'${\rm angle}(\psi)$')
-#     plt.savefig('quantumspectral.png',dpi=240)
-#     plt.show()
-
-    return psiFieldSlices, contrastVals, phases
+    return psi, contrastVals, psiFieldSlices[:,:,:]
 
 
 
